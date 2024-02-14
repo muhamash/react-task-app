af78fb6dfb0cefa3bfc8f753936866448d040efb
muhamash <ashraful@xpeedlab.tech>
2024-02-15 00:36:39 +0600

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import Hero from './components/Hero'
 import Nav from './components/Nav'
 
 function App() {
   return (
     <div>
-      <Nav/>
+      <div>
+        <div>
+          <Nav/>
+        </div>
+        <div className="pb-[114px] pt-20 md:mt-[100px] flex justify-center">
+          <Hero/>
+        </div>
+      </div>
     </div>
   )
 }
