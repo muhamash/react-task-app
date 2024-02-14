23aa6f5d379fa1f5039b482fc59829612f601d68
muhamash <ashraful@xpeedlab.tech>
2024-02-15 03:59:13 +0600

    brush up favState avoided re render

--- src/components/Table/TableDataRow.jsx
+++ src/components/Table/TableDataRow.jsx
@@ -1,17 +1,17 @@
+import FavState from "./FavState";
 import Tags from "./Tags";
 
 const TableDataRow = ({title, description, tags}) =>
-{
+{   
+    // const [ favorite, setFavorite ] = useState( false );
+    // const handleFavorite = () =>
+    // {
+    //     setFavorite( prevFavorite => !prevFavorite );
+    // };
     return (
         <tr className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
-            <td>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-star" width="24"
-                    height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="yellow" fill="yellow"
-                    strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path
-                        d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                </svg>
+            <td className="cursor-pointer">
+                <FavState onClick={(isFavorite)=>{console.log(isFavorite)} }/>
             </td>
             <td>{ title }</td>
             <td>
--- src/components/Table/Tags.jsx
+++ src/components/Table/Tags.jsx
@@ -12,7 +12,7 @@ const Tags = ({ tags }) => {
                         style={{
                             backgroundColor: colors[Math.floor(Math.random() * colors.length)],
                         }}
-                        className={`inline-block h-5 whitespace-nowrap rounded-[45px] px-2.5 text-sm capitalize text-[#F4F5F6]`}>
+                        className={`inline-block h-5 whitespace-nowrap rounded-[45px] mx-[3px] px-2.5 text-sm capitalize text-[#F4F5F6]`}>
                         {tag}
                     </span>
                 </React.Fragment>
