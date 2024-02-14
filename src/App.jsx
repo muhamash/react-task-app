f5b992cf74c11d70b5ccc59083b8a5112d561cf2
muhamash <ashraful@xpeedlab.tech>
2024-02-15 01:52:07 +0600

    all components added..

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import './App.css'
-import Hero from './components/Hero'
-import Nav from './components/Nav'
+import './App.css';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Nav from './components/Nav';
+import Table from './components/Table/Table';
 
 function App() {
   return (
-    <div>
+    <div className='bg-[#707375] font-[Inter] text-white h-full'>
       <div>
         <div>
-          <Nav/>
+          <Nav />
         </div>
         <div className="pb-[114px] pt-20 md:mt-[100px] flex justify-center">
-          <Hero/>
+          <Hero />
         </div>
       </div>
+
+      <div className='container mx-auto' id='tasks'>
+          <Table/>
+      </div>
+
+      <div>
+        <Footer/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
